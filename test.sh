1900091d2f80ba0a9ce5ebe3f6f03264b6a6d7cf
ptprashanttripathi <ptprashanttripathi@outlook.com>
2022-04-22 12:05:17 +0530

    Added: Tests

---
+++ test.sh
@@ -0,0 +1,11 @@
+#/usr/bin/bash
+node ./main ./sample/sutra.ved
+node ./main ./sample/paryantam.ved 
+node ./main ./sample/viram.ved 
+node ./main ./sample/simpleinterest.ved 
+node ./main ./sample/yadiath.ved 
+node ./main ./sample/chakram.ved 
+node ./main ./sample/suchi.ved
+node ./main ./sample/avahan.ved
+node ./main ./sample/operators.ved
+node ./main ./sample/nirdesa.ved
