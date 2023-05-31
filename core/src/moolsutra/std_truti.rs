c5b572b8d1042f82197f876b5ba0090c83e0214f
PtPrashantTripathi <ptprashanttripathi@outlook.com>
2023-05-31 19:34:24 +0530

    Created :  moolsutra truti

---
+++ core/src/moolsutra/std_truti.rs
@@ -0,0 +1,15 @@
+use super::super::aadhaar::Aadhaar;
+use super::super::dosasucana::Dosa;
+use super::super::mulya::Mulya;
+
+/// function truti त्रुटि : (string) -> dosa
+/// this function throws an dosa with the given string
+pub fn truti(aadhaar: &mut Aadhaar, from: usize) -> Result<Mulya, Dosa> {
+    let args = &aadhaar.rashi[from..aadhaar.rashi_len()];
+    if let Some(arg) = args.get(0) {
+        let dosa = format!("त्रुटि : {arg}");
+        Err(aadhaar.throw_dosa(&dosa))
+    } else {
+        Err(aadhaar.throw_dosa("त्रुटि"))
+    }
+}
