e439de706733eeb3db00d60534ee25632759db45
PtPrashantTripathi <ptprashanttripathi@outlook.com>
2023-05-31 19:33:50 +0530

    Created :  moolsutra shabd

---
+++ core/src/moolsutra/std_shabd.rs
@@ -0,0 +1,15 @@
+use super::super::aadhaar::Aadhaar;
+use super::super::dosasucana::Dosa;
+use super::super::mulya::Mulya;
+
+/// function shabd शब्द : (string) -> string
+/// this function returns the given string
+pub fn shabd(aadhaar: &mut Aadhaar, from: usize) -> Result<Mulya, Dosa> {
+    let args = &aadhaar.rashi[from..aadhaar.rashi_len()];
+    let arg = args.get(0);
+    if let Some(arg) = arg {
+        Ok(Mulya::Vakya(aadhaar.gc.intern(arg.to_string())))
+    } else {
+        Err(aadhaar.throw_dosa("Invalid argument to string Function"))
+    }
+}
