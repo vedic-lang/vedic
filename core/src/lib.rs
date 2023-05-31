f5940d6493a40efc00dadc3c6a92faec32a659e4
PtPrashantTripathi <ptprashanttripathi@outlook.com>
2023-05-31 19:22:25 +0530

    Created : initial core lib

---
+++ core/src/lib.rs
@@ -0,0 +1,17 @@
+pub mod aadesh;
+pub mod aadhaar;
+pub mod anupurva;
+pub mod dosa_nirupaka;
+pub mod dosasucana;
+pub mod fetchsource;
+pub mod lexer;
+pub mod logger;
+pub mod moolsutra;
+pub mod mulya;
+pub mod mulyankan;
+pub mod pada;
+pub mod parser;
+pub mod rules;
+pub mod smrti;
+pub mod smrti_prabandhan;
+pub mod vastuni;
